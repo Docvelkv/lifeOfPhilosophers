33c7f8c4f2fb932a07ac8a73c748e707a968775f
Docvelkv <docvel@rambler.ru>
2024-01-16 02:14:42 +0300

    Решение - один из философов (всё равно какой) берёт вилки в обратном порядке.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,17 @@ public class Main {
         for (int i = 0; i < n; i++) {
             Fork leftFork = forks[i];
             Fork rightFork = forks[(i + 1) % n];
-            philosophicalCommunity[i] = new Philosopher(leftFork, rightFork);
-            Thread thread = new Thread(philosophicalCommunity[i], String.format("Философ %d", i + 1));
-            thread.start();
+            // Первый философ берёт вилки в обратном порядке
+            if(i == 0){
+                philosophicalCommunity[i] = new Philosopher(rightFork, leftFork);
+                Thread thread = new Thread(philosophicalCommunity[i], String.format("Философ %d", i + 1));
+                thread.start();
+            }
+            else{
+                philosophicalCommunity[i] = new Philosopher(leftFork, rightFork);
+                Thread thread = new Thread(philosophicalCommunity[i], String.format("Философ %d", i + 1));
+                thread.start();
+            }
         }
     }
 }
